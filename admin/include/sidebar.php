e183b5ad7a1504bffa5f8ef0cd66bb3b9a277bb1
nusratalam123 <nusratalam975@gmail.com>
2023-07-02 00:10:58 +0600

    first commit

---
+++ admin/include/sidebar.php
@@ -0,0 +1,139 @@
+<?php
+include('connectionf/bdf.php');
+//session_start();
+$query=mysqli_query($conn,"select * from admin_login where admin_email='{$_SESSION['email']}' and admin_type='1'");
+
+ if(mysqli_num_rows($query)>0){
+  ?>
+  <div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+      <h3 style="padding:10px">Super Admin</h3>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="admin-dashbroad.php">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+           </li>
+           <li>
+            <a class="nav-link active" href="../homef/homef.php">
+              <span data-feather="home"></span>
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_mess_page.php">
+              <span data-feather="home"></span>
+              Mess
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_mess_approve.php">
+              <span data-feather="home"></span>
+              Mess Approval page
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_flat.php">
+              <span data-feather="home"></span>
+              Flat
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_women.php">
+              <span data-feather="users"></span>
+              Women Chef
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_vehicle_page.php">
+              <span data-feather="bar-chart-2"></span>
+              Vehicle
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="customer.php">
+              <span data-feather="users"></span>
+              Customers
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="booking.php">
+              <span data-feather="bar-chart-2"></span>
+               Booked List
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_review_page.php">
+              <span data-feather="users"></span>
+              Review Page
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+<?php
+ }
+ else{
+  ?>
+ 
+  <div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+      <h3 style="padding:10px">User</h3>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="../admin/admin-dashbroad.php">
+              <span data-feather="home"></span>
+              
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="../homef/homef.php">
+              <span data-feather="home"></span>
+              
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="mess_page.php">
+              <span data-feather="home"></span>
+              Mess
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="flat_page.php">
+              <span data-feather="home"></span>
+              Flat
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="women_chef_page.php">
+              <span data-feather="users"></span>
+              Women Chef
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="vehicle_page.php">
+              <span data-feather="layers"></span>
+              Vehicle
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="see_review.php">
+              <span data-feather="layers"></span>
+              Review Page
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+<?php
+ }
+
+?>
+
---
+++ admin/user_dashbroad.php
@@ -0,0 +1,148 @@
+<?php
+ session_start();
+if($_SESSION['email']==true){
+ 
+}
+else{
+     header('location:../loginf/loginpage.php');
+}
+?>
+<?php
+include('include/header.php');
+include('include/sidebar.php');
+?>
+
+   
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <h1 class="h2">Dashboard</h1>
+          </div>
+          <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Mess page </h5>
+        <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <h6>Search Mess</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Add Post</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Booked Mess</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>finding easily with google map</h6>
+  </ol>
+</nav>
+<h4>Rules</h4>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Flat Page</h5>
+        <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <h6>Search Flat</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Add Post</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Booked Flat</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Finding easily with google map</h6>
+  </ol>
+</nav>
+<h4>Rules</h4>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+
+        <a href="#" class="btn btn-primary">Go somewhere\</a>
+      </div>
+    </div>
+  </div>
+</div><div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Chef Page</h5>
+        <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <h6>Search Vehicle</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Add Post</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Booked vehicle</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Finding easily with salary range</h6>
+  </ol>
+</nav>
+<h4>Rules</h4>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+      <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <h6>Search Flat</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Add Post</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Booked Flat</h6>
+  </ol>
+  <ol class="breadcrumb">
+    <h6>Finding easily with google map</h6>
+  </ol>
+</nav>
+<h4>Rules</h4>
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+          
+          </div>
+        </main>
+      </div>
+    </div>
+
+   
+  </body>
+</html>
